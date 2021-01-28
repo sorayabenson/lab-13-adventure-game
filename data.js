51beba8b54dd77b45a10bee025d786c88700e006
Soraya Benson <soraya.benson@gmail.com>
2021-01-27 17:49:05 -0800

    data.js filled out

---
+++ data.js
@@ -0,0 +1,101 @@
+const bedroom = {
+    id: 'bedroom',
+    title: 'The Bedroom',
+    /*map: {
+        top: ,
+        left: ,
+    },*/
+    image: 'bedroom.png',
+    description: `You seek rest but only find insomnia, who do you ask for help?`,
+    choices: [{
+        id: 'opossum',
+        image: 'opossum-ghost.png',
+        description: `Find deep sleep after they teach you how to play dead!`,
+        result: `Your eyes can finally stay shut until morning.`,
+        light: 10,
+        friendlyGhosts: 1,
+    }, {
+        id: 'blanket',
+        image: 'blanket-ghost.png',
+        description: `Get wrapped up in a weighted blanket like no other.`,
+        result: `You fall into a deep slumber that you haven’t known in years.`,
+        light: 30,
+        friendlyGhosts: 5,
+    }, {
+        id: 'screen',
+        image: 'screen-ghost.png',
+        description: `Turn off your brain and turn on that screen.`,
+        result: `Oh no, it’s the Endless Scroll Shapeshifter! They’ve trapped you in an endless loop of doom scrolling and steal the little peace you had!`,
+        light: -20,
+        friendlyGhosts: -3,
+    }]
+};
+
+const kitchen = {
+    id: 'kitchen',
+    title: 'The Kitchen',
+    /*map: {
+        top: ,
+        left: ,
+    },*/
+    image: 'kitchen.png',
+    description: `You seek nourishment but find the kitchen in utter disarray, who do you ask for help?`,
+    choices: [{
+        id: 'coffee',
+        image: 'coffee-ghost.png',
+        description: `Enjoy the hot cup of energy and get out of the kitchen fast.`,
+        result: `Oh no, it’s the Nerve Fraying Shapeshifter! They betray you and your nerves, you can’t stop shaking!`,
+        light: -15,
+        friendlyGhosts: -2,
+    }, {
+        id: `dishes`,
+        image: 'dishes-ghost.png',
+        description: `Watch the dishes do themselves.`,
+        result: `The kitchen sparkles and you dance in the sheer joy that you didn’t clean it yourself.`,
+        light: 30,
+        friendlyGhosts: 3,
+    }, {
+        id: 'pizza',
+        image: 'pizza-ghost.png',
+        description: ` They get you and your turtle your favorite snack.`,
+        result: `You enjoy food you didn’t have to make yourself.`,
+        light: 15,
+        friendlyGhosts: 2,
+    }]
+};
+
+const shower = {
+    id: 'shower',
+    title: 'The Shower',
+    /*map: {
+        top: ,
+        left: ,
+    },*/
+    image: 'shower.png',
+    description: `You seek a cleansing reset, but not even the deluge of hot water can wash away the exhaustion, who do you ask for help?`,
+    choices: [{
+        id: 'plant',
+        title: 'Plant Ghost',
+        image: 'plant-ghost.png',
+        description: `Bathe in a lush forest of well cared for plants that you don’t have to remember to water.`,
+        result: `Your shower turns into an absolute oasis.`,
+        light: 20,
+        friendlyGhosts: 3,
+    }, {
+        id: 'essentialOil',
+        title: 'Essential Oil Ghost',
+        image: 'essential-oil-ghost.png',
+        description: `Bring the aroma therapy game to the next level.`,
+        result: `Oh no, it’s the Slippery Shapeshifter! You’re trapped in the shower with no traction, flailing desperately to escape!`,
+        light: -20,
+        friendlyGhosts: -2,
+    }, {
+        id: 'smoke',
+        title: 'Smoke Ghost'
+        image: 'smoke-ghost.png',
+        description: `Find even more comfort in relaxing billows of smoke.`,
+        result: `Your mind finds the stillness it has been longing for.`,
+        light: 25,
+        friendlyGhosts: 5,
+    }]
+};
